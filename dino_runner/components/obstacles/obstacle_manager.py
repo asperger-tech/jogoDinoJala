13dc3fa82e0ba62b3642f91627c7d76588b90601
Cristian S. Neves <crislife987@gmail.com>
2023-03-19 16:37:29 -0300

    Update obstacle_manager.py

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,26 +1,34 @@
 import pygame
+import random
+from dino_runner.components.obstacles.cactus import Cactus, CactusLarge
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.utils.constants import * 
 
-from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
-    
+
     def update(self, game):
+
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
-        
+            random_obstacle = random.randint(0, 2) 
+
+            if random_obstacle == 0:            
+                self.obstacles.append(Cactus(SMALL_CACTUS))
+            elif random_obstacle == 1:
+                self.obstacles.append(CactusLarge(LARGE_CACTUS))
+            elif random_obstacle == 2:
+                self.obstacles.append(Bird(BIRD))
+
+
         for obstacle in self.obstacles:
-            obstacle.update(game.game_speed, self.obstacles)
-            
+            obstacle.update(game.game_speed, self.obstacles)    #mover para esquerda e deleta.
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
-                break                
+                break
     
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
-
-
