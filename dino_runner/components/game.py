70dd1f83b384285eacb16e3028e30db764d98524
Cristian S. Neves <crislife987@gmail.com>
2023-03-20 19:29:29 -0300

    Update game.py

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -13,7 +13,7 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
-        self.game_speed = 15
+        self.game_speed = 25
         self.x_pos_bg = 0
         self.y_pos_bg = 380
 
