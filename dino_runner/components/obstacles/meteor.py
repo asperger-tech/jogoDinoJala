cd6bfe7060f9c23f45ff65de81485d95741b1623
Cristian S. Neves <crislife987@gmail.com>
2023-03-22 14:39:53 -0300

    Update meteor.py

--- dino_runner/components/obstacles/meteor.py
+++ dino_runner/components/obstacles/meteor.py
@@ -3,7 +3,7 @@ import pygame
 from dino_runner.components.obstacles.obstacle import Obstacle 
 
 class Meteor(Obstacle):
-    def __init__(self, images): #images[0,1]
+    def __init__(self, images): 
         self.type = 0
         super().__init__(images, self.type)
         self.rect.y = random.randint(140,230)
@@ -12,7 +12,7 @@ class Meteor(Obstacle):
     def draw(self, screen):
         current_time = pygame.time.get_ticks()
 
-        if current_time - self.explosion > 200:  # tempo em milissegundos para explodir
+        if current_time - self.explosion > 200: 
             self.explosion = current_time
             screen.blit(self.images[self.type], (self.rect.x, self.rect.y))
             self.type += 1
