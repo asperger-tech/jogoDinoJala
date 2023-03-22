cf9189f5e319734bc1a8248528d3e46030366665
Cristian S. Neves <crislife987@gmail.com>
2023-03-22 19:14:01 -0300

    Update cactus.py

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,21 +1,13 @@
-from dino_runner.components.obstacles.obstacle import Obstacle 
+from dino_runner.components.obstacles.obstacle import * 
 import random
 
 class Cactus(Obstacle):
-    def __init__(self, images):
+    def __init__(self, images,y):
     
         self.type = random.randint(0,2)
         super().__init__(images, self.type)
         
-        self.rect.y = 320
-
-class CactusLarge(Obstacle):
-    def __init__(self, images):
-    
-        self.type = random.randint(0,2)
-        super().__init__(images, self.type)
-        
-        self.rect.y = 300    
+        self.rect.y = y
 
         
 
