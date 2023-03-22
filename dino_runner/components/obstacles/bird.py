e70527547292a0b69d8925ce49d550b97f32ca21
Cristian S. Neves <crislife987@gmail.com>
2023-03-22 14:39:51 -0300

    Update bird.py

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -9,11 +9,13 @@ class Bird(Obstacle):
         super().__init__(images, self.type)
         self.rect.y = random.randint(140,250)
         self.last_flap_time = 0
+        self.moving = random.randint(1,2) % 2 == 0 and True or False
+        self.touched_ground = False
 
     def draw(self, screen):
         current_time = pygame.time.get_ticks()
 
-        if current_time - self.last_flap_time > 200:  # tempo em milissegundos para bater as asas
+        if current_time - self.last_flap_time > 200:  
             self.last_flap_time = current_time
             screen.blit(self.images[self.type], (self.rect.x, self.rect.y))
             self.type += 1
@@ -21,6 +23,20 @@ class Bird(Obstacle):
                 self.type = 0
         else:
             screen.blit(self.images[self.type], (self.rect.x, self.rect.y))
+    
+    def update(self,game_speed, obstacles):
+        if self.moving:
+            if not self.touched_ground:
+                self.rect.y +=7
+                if self.rect.y > 330:
+                    self.touched_ground = True
+            else:
+                self.rect.y -=7
+                if self.rect.y < 200:
+                    self.touched_ground = False
+        
+        super().update(game_speed, obstacles)
+    
     
           
 
