60c6bf4959c87ab320363e0c375e3f6c6f205fca
Cristian S. Neves <crislife987@gmail.com>
2023-03-22 14:39:47 -0300

    Update dinosaur.py

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,6 +1,11 @@
 import pygame
 
-from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
+from dino_runner.utils.constants import *
+from dino_runner.components.power_ups.shield import *
+
+DUCK_IMG = {DEFAULT_TYPE: DUCKING, SHIELD_TYPE:DUCKING_SHIELD, HAMMER_TYPE:DUCKING_HAMMER}
+JUMP_IMG = {DEFAULT_TYPE: JUMPING, SHIELD_TYPE:JUMPING_SHIELD, HAMMER_TYPE:JUMPING_HAMMER}
+RUN_IMG = {DEFAULT_TYPE: RUNNING, SHIELD_TYPE:RUNNING_SHIELD, HAMMER_TYPE:RUNNING_HAMMER}
 
 X_POS = 80
 Y_POS = 310
@@ -8,7 +13,8 @@ JUMP_VEL = 8.5
 
 class Dinosaur:
     def __init__(self):
-        self.image = RUNNING[0]
+        self.type = DEFAULT_TYPE
+        self.image = RUN_IMG[self.type][0]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
@@ -17,16 +23,19 @@ class Dinosaur:
         self.dino_duck = False
         self.step_index = 0
         self.jump_vel = JUMP_VEL
+
+        self.has_power_up = False
+
     
     def run(self):
-        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1] #ternary operator 
+        self.image = RUN_IMG[self.type][0] if self.step_index < 5 else RUN_IMG[self.type][1] 
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
         self.step_index+=1        
        
     def jump(self):
-        self.image = JUMPING
+        self.image = JUMP_IMG[self.type]
         if self.dino_jump:
             self.dino_rect.y -= self.jump_vel*4
             self.jump_vel -=0.8
@@ -37,7 +46,7 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL
     
     def duck(self):
-        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.image = DUCK_IMG[self.type][0] if self.step_index < 5 else DUCK_IMG[self.type][1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS + 35
@@ -57,6 +66,12 @@ class Dinosaur:
             self.jump_vel -= 1.5
         elif not self.dino_duck and not self.dino_jump:
             self.dino_run = True
+        
+        if user_input[pygame.K_RIGHT]:
+            self.dino_rect.x +=10
+            
+        elif user_input[pygame.K_LEFT]:
+            self.dino_rect.x -=10
 
         if self.dino_run:
             self.run()
