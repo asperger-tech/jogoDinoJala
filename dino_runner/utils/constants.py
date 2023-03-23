f3bdfc7ce667eff377aef8e7f75c7471ff920cb8
Cristian S. Neves <crislife987@gmail.com>
2023-03-23 14:46:13 -0300

    Update constants.py

--- dino_runner/utils/constants.py
+++ dino_runner/utils/constants.py
@@ -13,8 +13,12 @@ SHIELD_TYPE = "shield"
 HAMMER_TYPE = "hammer"
 
 ICON = pygame.image.load(os.path.join(IMG_DIR, "DinoWallpaper.png"))
+pygame.mixer.init()
+SOUNDTRACK = pygame.mixer.Sound('dino_runner/assets/Sounds/soundtrack.mp3')
+SOUNDTRACK.set_volume(0.5)
+DEATHSOUND = pygame.mixer.Sound('dino_runner/assets/Sounds/death.wav')
+HITSOUND = pygame.mixer.Sound('dino_runner/assets/Sounds/hit01.wav')
 
-SOUNDTRACK = 'dino_runner/assets/Sounds/soundtrack.mp3'
 
 RUNNING = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1.png")),
@@ -50,63 +54,51 @@ DUCKING_HAMMER = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
 ]
 
-SMALL_CACTUS = [
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus2.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus3.png")),
-]
-LARGE_CACTUS = [
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus2.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus3.png")),
+PLANT = [
+    pygame.image.load(os.path.join(IMG_DIR, "Monsters/planta1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Monsters/planta2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Monsters/planta3.png"))
 ]
 SKELETON = [
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/esqueleto.png"))
+    pygame.image.load(os.path.join(IMG_DIR, "Monsters/esqueleto1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Monsters/esqueleto2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Monsters/esqueleto3.png"))
+
 ]
 MINOTAUR = [
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/0.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/1.png"))
-]
-SLIME = [
-        pygame.image.load(os.path.join(IMG_DIR, "Other/slime.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Monsters/minotaur1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Monsters/minotaur2.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Monsters/minotaur3.png"))
 ]
 
-BIRD = [
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird2.png")),
-]
-BIRD_BLUE = [
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird1blue.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird2blue.png")),
-]
-BIRD_GREEN = [
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird1green.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird2green.png")),
-]
-GHOST = [
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/ghost2.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/ghost.png"))
+BAT = [
+    pygame.image.load(os.path.join(IMG_DIR, "Bird/bat1.png")),
+    pygame.image.load(os.path.join(IMG_DIR, "Bird/bat2.png"))
     ]
 
 METEOR = [
     pygame.image.load(os.path.join(IMG_DIR, "Other/0.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Other/1.png")),
 ]
-BIRD_LIST = [BIRD,BIRD_BLUE,BIRD_GREEN]
 
 LOGODINORUN = pygame.image.load(os.path.join(IMG_DIR, 'Other/logo.png'))
 half_screen_height = SCREEN_HEIGHT // 2
 half_screen_width = SCREEN_WIDTH // 2
-LOSE_TEXT = pygame.image.load(os.path.join(IMG_DIR, 'Other/GameOver.png'))
-
-
-
+LOSE_TEXT = pygame.image.load(os.path.join(IMG_DIR, 'Other/gameovertest2.png'))
 
 CLOUD = pygame.image.load(os.path.join(IMG_DIR, 'Other/Cloud.png'))
 SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png'))
 HAMMER = pygame.image.load(os.path.join(IMG_DIR, 'Other/hammer.png'))
 
 BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/teste3.jpg'))
+BG_MENU = pygame.image.load(os.path.join(IMG_DIR, 'Other/backmenu2.jpg'))
+COIN = [
+    pygame.image.load(os.path.join(IMG_DIR, 'Other/coin/star1.png')),
+    pygame.image.load(os.path.join(IMG_DIR, 'Other/coin/star2.png')),
+    pygame.image.load(os.path.join(IMG_DIR, 'Other/coin/star3.png')),
+    pygame.image.load(os.path.join(IMG_DIR, 'Other/coin/star5.png')),
+    pygame.image.load(os.path.join(IMG_DIR, 'Other/coin/star6.png'))
+]
 
 HEART = pygame.image.load(os.path.join(IMG_DIR, 'Other/heart.png'))
 
