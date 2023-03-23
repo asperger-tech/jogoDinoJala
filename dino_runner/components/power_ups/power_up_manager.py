cdf26e91428b7a0e7523c7bd88a689cadcbfc770
Cristian S. Neves <crislife987@gmail.com>
2023-03-23 02:00:18 -0300

    Update power_up_manager.py

--- dino_runner/components/power_ups/power_up_manager.py
+++ dino_runner/components/power_ups/power_up_manager.py
@@ -2,16 +2,22 @@ import random
 import pygame
 from dino_runner.components.power_ups.shield import Shield
 from dino_runner.components.power_ups.hammer import Hammer
+from dino_runner.components.power_ups.extra_life import Heart
+from dino_runner.components.game import *
+
+
 
 
 class PowerUpManager:
     def __init__(self):
         self.power_ups = []
+        self.extra_life = []
         self.when_appears = 0
+        self.heart_appears = 100
     
     def generate_power_up(self, score):
         if len(self.power_ups) == 0 and self.when_appears == score:
-            self.when_appears += random.randint(300,400)
+            self.when_appears += random.randint(300,500)
 
             randomic_choice = random.randint(0,1)
 
@@ -19,8 +25,11 @@ class PowerUpManager:
                 self.power_ups.append(Shield())
             elif randomic_choice == 1:
                 self.power_ups.append(Hammer())
-            
-            
+
+        if len(self.extra_life) == 0 and self.heart_appears == score:
+            self.heart_appears += random.randint(300,500)
+            self.extra_life.append(Heart())
+
     def update(self, game):
         self.generate_power_up(game.score)
         
@@ -35,11 +44,28 @@ class PowerUpManager:
                 player.type = power_up.type 
                 player.power_up_time_up = power_up.start_time + (power_up.duration * 1000)
                 self.power_ups.remove(power_up)
-    
+
+        for heart in self.extra_life:
+            heart.update(game.game_speed, self.extra_life)
+
+            player = game.player
+            if player.dino_rect.colliderect(heart.rect):
+                self.extra_life.remove(heart)
+                game.lifes +=1
+        
+        if game.playing == False:
+            self.when_appears = 0
+            self.heart_appears = 100
     
     def draw(self, screen):
         for power_up in self.power_ups:
             power_up.draw(screen)
+        
+        for heart in self.extra_life:
+            heart.draw(screen)
     
     def reset_power_ups(self):
-        self.power_ups.clear()            
+        self.power_ups.clear()   
+    def reset_spawn(self):
+        self.when_appears = 0
+        self.heart_appears = 0
